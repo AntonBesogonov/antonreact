839b4e61887753ce33f8906c4978423b79eb4639
AntonBesogonov <anton_besogonov@bk.ru>
2022-05-06 10:24:35 +0500

    1

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,39 +1,17 @@
+import axios from "axios";
 import React from "react";
 import Us from './Users.module.css';
+import userPhoto from '../../assets/images/photo-icon.png'
 
 
 let Users = (props) => {
      if (props.users.length === 0) {
-          props.setUsers([
-               {
-                    id: 1,
-                    photoUrl:
-                         'https://bugaga.ru/uploads/posts/2016-01/1453106278_znamenitosti-dva-v-odnom-10.jpg',
-                    followed: false,
-                    fullName: 'Dmitry',
-                    status: 'I am boss 1',
-                    location: { city: 'Minsk', country: 'Belarus' },
-               },
-               {
-                    id: 2,
-                    photoUrl:
-                         'https://bugaga.ru/uploads/posts/2016-01/1453106278_znamenitosti-dva-v-odnom-10.jpg',
-                    followed: true,
-                    fullName: 'Sasha',
-                    status: 'I am boss 2',
-                    location: { city: 'Moscow', country: 'Russia' },
-               },
-               {
-                    id: 3,
-                    photoUrl:
-                         'https://bugaga.ru/uploads/posts/2016-01/1453106278_znamenitosti-dva-v-odnom-10.jpg',
-                    followed: false,
-                    fullName: 'Andrew',
-                    status: 'I am boss 3',
-                    location: { city: 'Kiev', country: 'Ukraine' },
-               },
-          ]
-          )
+          axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+               
+               props.setUsers(response.data.items)
+          })
+          
+          
 
      };
 
@@ -44,7 +22,7 @@ let Users = (props) => {
                     props.users.map(u => <div key={u.id}>
                          <span>
                               <div>
-                                   <img src={u.photoUrl} className={Us.photo}></img>
+                                   <img src={u.photos.small != null ? u.photos.small : userPhoto } className={Us.photo}></img>
                               </div>
                               <div>
                                    {u.followed
@@ -54,12 +32,12 @@ let Users = (props) => {
                          </span>
                          <span>
                               <span>
-                                   <div>{u.fullName}</div>
+                                   <div>{u.name}</div>
                                    <div>{u.status}</div>
                               </span>
                               <span>
-                                   <div>{u.location.country}</div>
-                                   <div>{u.location.city}</div>
+                                   <div>{'u.location.country'}</div>
+                                   <div>{'u.location.city'}</div>
                               </span>
                          </span>
                     </div>)
