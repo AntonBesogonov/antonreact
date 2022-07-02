ba0eb4c2530f6c39d308fa2dde3edc4648539b4a
AntonBesogonov <anton_besogonov@bk.ru>
2022-07-03 02:06:49 +0500

    не грузит профиль

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -12,6 +12,3 @@ const Profile = (props) => {
 };
 
 export default Profile;
-
-
-
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,58 +1,15 @@
-// import React from 'react';
-// import { connect } from 'react-redux';
-// import Profile from './Profile';
-// import axios from "axios";
-// import { setUserProfile } from '../redux/profile-reducer';
-// import { useParams } from 'react-router-dom';
-// import { useState, useEffect } from 'react';
-
-// const blo = () => {
-//   const [profile.setPost] = useState([]);
-// }
-// useEffect(() => {
-//   fetch('https://social-network.samuraijs.com/api/1.0/profile/2')
-//     .then(response => response.json())
-//     .then(commits => alert(userId));
-// }, []);
-
-// return (
-//   <div>
-//     <h1>qwe</h1>
-//     {
-//       post.map(post =>
-//         <Link key={post.id} to={`/profile/${post.id}`}></Link>
-//         )
-//     }
-//   </div>
-// )
-
-
-
-
-
-
-// export default ProfileContainer;
-
-
-
-
-
 import React from 'react';
 import { connect } from 'react-redux';
 import Profile from './Profile';
 import axios from "axios";
 import { setUserProfile } from '../redux/profile-reducer';
 
-
 class ProfileContainer extends React.Component {
 
   componentDidMount() {
-    let userId = this.props.match.params.userId
-    if (!userId) {
-      userId = 2;
-    }
     axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
       .then(response => {
+        
         this.props.setUserProfile(response.data);
       });
   }
@@ -69,9 +26,4 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile
 });
 
-
-
 export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
-
-
-
