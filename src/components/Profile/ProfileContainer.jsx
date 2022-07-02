3a2586feee0431ff3355650e23ae48c3171d0547
AntonBesogonov <anton_besogonov@bk.ru>
2022-07-03 01:58:46 +0500

    withRouter нет в 6 версии реакт

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,77 +1,77 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Profile from './Profile';
-import axios from "axios";
-import { setUserProfile } from '../redux/profile-reducer';
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-
-const blo = () => {
-  const [profile.setPost] = useState([]);
-}
-useEffect(() => {
-  fetch('https://social-network.samuraijs.com/api/1.0/profile/2')
-    .then(response => response.json())
-    .then(commits => alert(userId));
-}, []);
-
-return (
-  <div>
-    <h1>qwe</h1>
-    {
-      post.map(post =>
-        <Link key={post.id} to={`/profile/${post.id}`}></Link>
-        )
-    }
-  </div>
-)
- 
-
-
-
-
-
-export default ProfileContainer;
-
-
-
-
-
 // import React from 'react';
 // import { connect } from 'react-redux';
 // import Profile from './Profile';
 // import axios from "axios";
 // import { setUserProfile } from '../redux/profile-reducer';
-// import { withRouter } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
+// import { useState, useEffect } from 'react';
 
-// class ProfileContainer extends React.Component {
-
-//   componentDidMount() {
-//     let userId = this.props.match.params.userId
-//     if (!userId) {
-//       userId = 2;
-//     }
-//     axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
-//       .then(response => {
-//         this.props.setUserProfile(response.data);
-//       });
-//   }
-
-//   render() {
-//     return (
-//       <Profile {...this.props} profile={this.props.profile} />
-//     )
-//   }
+// const blo = () => {
+//   const [profile.setPost] = useState([]);
 // }
+// useEffect(() => {
+//   fetch('https://social-network.samuraijs.com/api/1.0/profile/2')
+//     .then(response => response.json())
+//     .then(commits => alert(userId));
+// }, []);
+
+// return (
+//   <div>
+//     <h1>qwe</h1>
+//     {
+//       post.map(post =>
+//         <Link key={post.id} to={`/profile/${post.id}`}></Link>
+//         )
+//     }
+//   </div>
+// )
 
 
-// let mapStateToProps = (state) => ({
-//   profile: state.profilePage.profile
-// });
 
-// let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
-// export default connect(mapStateToProps, { setUserProfile })(WithUrlDataContainerComponent);
+
+
+// export default ProfileContainer;
+
+
+
+
+
+import React from 'react';
+import { connect } from 'react-redux';
+import Profile from './Profile';
+import axios from "axios";
+import { setUserProfile } from '../redux/profile-reducer';
+
+
+class ProfileContainer extends React.Component {
+
+  componentDidMount() {
+    let userId = this.props.match.params.userId
+    if (!userId) {
+      userId = 2;
+    }
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+      .then(response => {
+        this.props.setUserProfile(response.data);
+      });
+  }
+
+  render() {
+    return (
+      <Profile {...this.props} profile={this.props.profile} />
+    )
+  }
+}
+
+
+let mapStateToProps = (state) => ({
+  profile: state.profilePage.profile
+});
+
+
+
+export default connect(mapStateToProps, { setUserProfile })(ProfileContainer);
 
 
 
