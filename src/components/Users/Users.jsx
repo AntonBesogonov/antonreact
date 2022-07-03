ce6a89179b48fb0b34df9bad146da00dddbcbabe
AntonBesogonov <anton_besogonov@bk.ru>
2022-07-04 01:52:09 +0500

    API KEY выдает ошибку  - Some error occured

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Us from './Users.module.css';
 import userPhoto from '../../assets/images/photo-icon.png'
 import { NavLink } from "react-router-dom";
-
+import axios from "axios";
 
 let Users = (props) => {
 
@@ -11,27 +11,52 @@ let Users = (props) => {
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i);
   }
-  
+
   return (
     <div className={Us.users}>
-      
-        {pages.map(p => {
-          return <span className={props.currentPage === p && Us.selectedPage} onClick={(e) => { props.onPageChanged(p); }
-          }>{p}</span>
-        })};
-      
+
+      {pages.map(p => {
+        return <span className={props.currentPage === p && Us.selectedPage} onClick={(e) => { props.onPageChanged(p); }
+        }>{p}</span>
+      })};
+
       {
         props.users.map(u => <div key={u.id}>
           <span>
             <div>
               <NavLink to={'/profile/' + u.id}>
-              <img src={u.photos.small != null ? u.photos.small : userPhoto} className={Us.photo}></img>
+                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={Us.photo}></img>
               </NavLink>
             </div>
             <div>
               {u.followed
-                ? <button onClick={() => { props.unfollow(u.id) }} >UnFollow</button>
-                : <button onClick={() => { props.follow(u.id) }} >Follow</button>}
+                ? <button onClick={() => {
+                  axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/{$u.id}`, {
+                    withCredentials: true,
+                    headers: { "API-KEY": "" }
+                  })
+                    .then(response => {
+                      if (response.data.resultCode === 0) {
+                        props.unfollow(u.id);
+                      }
+
+                    });
+
+
+                }} >UnFollow</button>
+                : <button onClick={() => {
+                  axios.post(`https://social-network.samuraijs.com/api/1.0/follow/{$u.id}`, {}, {
+                    withCredentials: true,
+                    headers: { "API-KEY": "" }
+                  })
+                    .then(response => {
+                      if (response.data.resultCode === 0) {
+                        props.follow(u.id);
+                      }
+
+                    });
+
+                }} >Follow</button>}
             </div>
           </span>
           <span>
@@ -51,3 +76,5 @@ let Users = (props) => {
 }
 
 export default Users;
+
+
